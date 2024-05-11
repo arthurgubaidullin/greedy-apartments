1cb0caaf29af787dff9fef288a1ad9646b9a1c41
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-11 13:09:56 +0000

    Add function updateRealtorDocument

--- tenant-management/update-realtor-document/src/lib/update-realtor-document.spec.ts
+++
@@ -1,9 +0,0 @@
-import { updateRealtorDocumentInTenantManagement } from './update-realtor-document';
-
-describe('updateRealtorDocumentInTenantManagement', () => {
-  it('should work', () => {
-    expect(updateRealtorDocumentInTenantManagement()).toEqual(
-      'update-realtor-document-in-tenant-management'
-    );
-  });
-});
--- tenant-management/update-realtor-document/src/lib/update-realtor-document.ts
+++ tenant-management/update-realtor-document/src/lib/update-realtor-document.ts
@@ -1,3 +1,16 @@
-export function updateRealtorDocumentInTenantManagement(): string {
-  return 'update-realtor-document-in-tenant-management';
-}
+import * as RealtorRepository from '@ga/realtor-repository-in-tenant-management';
+import * as RealtorDocument from '@ga/realtor-document-in-tenant-management';
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
+
+const repository = RealtorRepository.getRealtorRepository();
+
+export const updateRealtorDocument = (document: RealtorDocument.T): void => {
+  return pipe(
+    repository.get(document.id),
+    O.fold(
+      () => repository.create(document),
+      () => repository.update(document)
+    )
+  );
+};
