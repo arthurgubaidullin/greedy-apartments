afe5ed2e625eade27e955c5bfee44a7c334c0b0d
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-11 12:58:30 +0000

    Add function getRealtorRepository

--- tenant-management/realtor-repository/src/lib/realtor-repository.spec.ts
+++
@@ -1,9 +0,0 @@
-import { realtorRepositoryInTenantManagement } from './realtor-repository';
-
-describe('realtorRepositoryInTenantManagement', () => {
-  it('should work', () => {
-    expect(realtorRepositoryInTenantManagement()).toEqual(
-      'realtor-repository-in-tenant-management'
-    );
-  });
-});
--- tenant-management/realtor-repository/src/lib/realtor-repository.ts
+++ tenant-management/realtor-repository/src/lib/realtor-repository.ts
@@ -1,3 +1,16 @@
-export function realtorRepositoryInTenantManagement(): string {
-  return 'realtor-repository-in-tenant-management';
-}
+import * as RealtorDocument from '@ga/realtor-document-in-tenant-management';
+import * as RealtorId from '@ga/realtor-id';
+
+const db = new Map<RealtorId.T, RealtorDocument.T>();
+
+export const getRealtorRepository = () => {
+  return {
+    create: (document: RealtorDocument.T): void => {
+      const id = document.id;
+
+      if (db.has(id)) {
+        db.set(id, document);
+      }
+    },
+  };
+};
