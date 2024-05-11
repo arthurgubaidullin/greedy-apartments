8421cb943b29a87c47c0df309ce6b2bd889a5e4c
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-11 08:37:59 +0000

    Add function registerRealtor

--- realtor-registry/register-realtor/src/lib/register-realtor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { registerRealtor } from './register-realtor';
-
-describe('registerRealtor', () => {
-  it('should work', () => {
-    expect(registerRealtor()).toEqual('register-realtor');
-  });
-});
--- realtor-registry/register-realtor/src/lib/register-realtor.ts
+++ realtor-registry/register-realtor/src/lib/register-realtor.ts
@@ -1,3 +1,8 @@
-export function registerRealtor(): string {
-  return 'register-realtor';
-}
+import * as RealtorDocument from '@ga/realtor-document-in-registry';
+import * as Realtor from '@ga/realtor-in-registry';
+
+export const registerRealtor = (
+  data: Realtor.Simplified
+): RealtorDocument.T => {
+  return Realtor.create(data);
+};
