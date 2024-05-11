740345789d103bf8de3e2e30bc7b54dc768b8090
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-11 09:03:54 +0000

    Rename project
    realtor-repository-api-in-registry → register-realtor-api-in-registry

--- realtor-registry/register-realtor-api/jest.config.ts
+++ realtor-registry/register-realtor-api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'realtor-repository-api-in-registry',
+  displayName: 'register-realtor-api-in-registry',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/realtor-registry/realtor-repository-api',
+  coverageDirectory: '../../coverage/realtor-registry/register-realtor-api',
 };
