58d074dc3cb71cb840baa003eeec1b611b3d0845
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-11 08:06:25 +0000

    Add project realtor-repository-api-in-registry

---
+++ realtor-registry/realtor-repository-api/src/lib/realtor-repository-api-in-registry.spec.ts
@@ -0,0 +1,9 @@
+import { realtorRepositoryApiInRegistry } from './realtor-repository-api-in-registry';
+
+describe('realtorRepositoryApiInRegistry', () => {
+  it('should work', () => {
+    expect(realtorRepositoryApiInRegistry()).toEqual(
+      'realtor-repository-api-in-registry'
+    );
+  });
+});
---
+++ realtor-registry/realtor-repository-api/src/lib/realtor-repository-api-in-registry.ts
@@ -0,0 +1,3 @@
+export function realtorRepositoryApiInRegistry(): string {
+  return 'realtor-repository-api-in-registry';
+}
