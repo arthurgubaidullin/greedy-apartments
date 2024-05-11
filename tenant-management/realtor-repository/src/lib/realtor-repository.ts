8ccfd6c679ba80f3fb8def48446b5d705dcab28c
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-11 13:09:24 +0000

    Fix function getRealtorRepository

--- tenant-management/realtor-repository/src/lib/realtor-repository.ts
+++ tenant-management/realtor-repository/src/lib/realtor-repository.ts
@@ -1,5 +1,6 @@
 import * as RealtorDocument from '@ga/realtor-document-in-tenant-management';
 import * as RealtorId from '@ga/realtor-id';
+import * as O from 'fp-ts/Option';
 
 const db = new Map<RealtorId.T, RealtorDocument.T>();
 
@@ -11,6 +12,12 @@ export const getRealtorRepository = () => {
       if (db.has(id)) {
         db.set(id, document);
       }
+    },
+    get: (id: RealtorId.T): O.Option<RealtorDocument.T> => {
+      return O.fromNullable(db.get(id));
+    },
+    update: (document: RealtorDocument.T) => {
+      db.set(document.id, document);
     },
   };
 };
