f9043f81ba32b34b8a0da23f4e597cd68298d3a3
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-11 08:53:17 +0000

    Add function getRealtorId

--- realtor-registry/realtor-document/src/lib/realtor-document.ts
+++ realtor-registry/realtor-document/src/lib/realtor-document.ts
@@ -1,7 +1,12 @@
 import * as Realtor from '@ga/realtor-in-registry';
-import { TypeOf } from 'io-ts';
+import * as t from 'io-ts';
+import * as RealtorId from '@ga/realtor-id';
+import { pipe } from 'fp-ts/lib/function';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface T extends TypeOf<typeof codec> {}
+export interface T extends t.TypeOf<typeof codec> {}
 
 export const codec = Realtor.codec;
+
+export const getRealtorId = (document: T): RealtorId.T =>
+  pipe(document, codec.encode, (a) => a.id);
