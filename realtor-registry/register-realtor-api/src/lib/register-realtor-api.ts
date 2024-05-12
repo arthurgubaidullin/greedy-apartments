0e32c0a32295beaf8a9fffa656072eb9d76345e6
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 14:23:07 +0000

    Fix api function

--- realtor-registry/register-realtor-api/src/lib/register-realtor-api.ts
+++ realtor-registry/register-realtor-api/src/lib/register-realtor-api.ts
@@ -4,9 +4,18 @@ import {
   registerRealtor,
 } from '@ga/register-realtor-in-registry';
 import { pipe } from 'fp-ts/function';
+import * as RealtorAdded from '@ga/realtor-added-observable-in-realtor-registry';
+import { chainFirst } from 'fp-ts/lib/Identity';
+import * as RealtorDocument from '@ga/realtor-document-in-registry';
 
 const repository = RealtorRepository.getRealtorRepository();
 
 export const registerRealtorApi = (data: RegisterRealtor): void => {
-  return pipe(data, registerRealtor, repository.create);
+  return pipe(
+    data,
+    registerRealtor,
+    chainFirst(repository.create),
+    RealtorDocument.toJSON,
+    RealtorAdded.publish
+  );
 };
