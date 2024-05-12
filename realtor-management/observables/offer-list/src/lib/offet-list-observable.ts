028be937aafcad33c3b0a0eaaa6fbd41ca5a958d
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 17:30:20 +0000

    Add observable offerList

--- realtor-management/observables/offer-list/src/lib/offet-list-observable.ts
+++ realtor-management/observables/offer-list/src/lib/offet-list-observable.ts
@@ -1,3 +1,12 @@
-export function offetListObservableInRealtorManagement(): string {
-  return 'offet-list-observable-in-realtor-management';
-}
+import * as O from 'fp-ts/Option';
+import { IObservableValue, observable } from 'mobx';
+import * as OfferStruct from '@ga/offet-struct-in-realtor-management';
+import { Eq } from 'fp-ts/Eq';
+
+type T = O.Option<OfferStruct.OfferStruct>;
+
+const _offerList = observable.box<T>(O.none, {
+  equals: O.getEq(OfferStruct.Eq).equals,
+} satisfies Eq<T>);
+
+export const offerList: Pick<IObservableValue<T>, 'get'> = _offerList;
