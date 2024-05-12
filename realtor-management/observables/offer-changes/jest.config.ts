f912146b82b54999dd3281fd7e9a8e10477d2a07
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 15:51:53 +0000

    Move & rename project
    offer-changes-in-realtor-management → offer-changes-observable-in-realtor-management

--- realtor-management/observables/offer-changes/jest.config.ts
+++ realtor-management/observables/offer-changes/jest.config.ts
@@ -1,11 +1,12 @@
 /* eslint-disable */
 export default {
-  displayName: 'offer-changes-in-realtor-management',
-  preset: '../../jest.preset.js',
+  displayName: 'offer-changes-observable-in-realtor-management',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/realtor-management/offer-changes',
+  coverageDirectory:
+    '../../../coverage/realtor-management/observables/offer-changes',
 };
