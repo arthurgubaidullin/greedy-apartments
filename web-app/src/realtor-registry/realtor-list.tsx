123c94bbf2e1c9d69cd74c5a42af57890641e1ad
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 14:05:22 +0000

    Add react component

--- web-app/src/realtor-registry/realtor-list.tsx
+++ web-app/src/realtor-registry/realtor-list.tsx
@@ -1,9 +1,39 @@
-export function RealtorList() {
+import * as RealtorId from '@ga/realtor-id';
+import { realtorList } from '@ga/realtor-list-observable-in-realtor-registry';
+import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+import { observer } from 'mobx-react-lite';
+
+export const RealtorList = observer(() => {
+  const list = pipe(
+    realtorList.get(),
+    O.fold(
+      () => <p>No realtors.</p>,
+      (list) => (
+        <ul>
+          {pipe(
+            list,
+            RA.map((item) => (
+              <li>
+                <div>
+                  id: {RealtorId.toString(item.id)}
+                  <br />
+                  name: {item.name}
+                </div>
+              </li>
+            ))
+          )}
+        </ul>
+      )
+    )
+  );
+
   return (
     <div>
       <h2>Realtor list</h2>
 
-      <ul></ul>
+      {list}
     </div>
   );
-}
+});
