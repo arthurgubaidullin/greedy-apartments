a9a5f7dc17f1dfd3fa2aad294b09383e837be321
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 13:32:22 +0000

    Add observable realtorAdded

--- realtor-registry/observables/realtor-added/src/lib/realtor-added-observable.ts
+++ realtor-registry/observables/realtor-added/src/lib/realtor-added-observable.ts
@@ -1,3 +1,19 @@
-export function realtorAddedObservableInRealtorRegistry(): string {
-  return 'realtor-added-observable-in-realtor-registry';
-}
+import * as O from 'fp-ts/Option';
+import { IObservableValue, action, observable } from 'mobx';
+import * as RealtorStruct from '@ga/realtor-struct-in-realtor-registry';
+import { Eq } from 'fp-ts/lib/Eq';
+
+type RealtorStructOption = O.Option<RealtorStruct.RealtorStruct>;
+
+const _realtorAdded = observable.box<RealtorStructOption>(O.none, {
+  equals: O.getEq(RealtorStruct.Eq).equals,
+} satisfies Eq<RealtorStructOption>);
+
+export const publish = action((realtor: RealtorStruct.RealtorStruct) => {
+  _realtorAdded.set(O.some(realtor));
+});
+
+export const realtorAdded: Pick<
+  IObservableValue<RealtorStructOption>,
+  'get'
+> = _realtorAdded;
