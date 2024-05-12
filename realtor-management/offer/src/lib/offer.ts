e76902d958ffca97fe91df51d69f92d01f7e7e67
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 08:30:50 +0000

    Add type Offer

--- realtor-management/offer/src/lib/offer.ts
+++ realtor-management/offer/src/lib/offer.ts
@@ -1,3 +1,26 @@
+import * as OfferId from '@ga/offer-id';
+import { pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
+import { fromNewtype } from 'io-ts-types';
+import * as Newtype from 'newtype-ts';
+
 export function offerInRealtorManagement(): string {
   return 'offer-in-realtor-management';
 }
+
+const Offer = t.strict({
+  id: OfferId.codec,
+  name: t.string,
+});
+
+export type SimplifiedOffer = t.TypeOf<typeof Offer>;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Offer
+  extends Newtype.Newtype<{ readonly Offer: unique symbol }, SimplifiedOffer> {}
+
+export const codec = fromNewtype<Offer>(Offer);
+
+const iso = Newtype.iso<Offer>();
+
+export const create = (data: SimplifiedOffer): Offer => pipe(data, iso.wrap);
