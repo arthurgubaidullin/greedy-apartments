1a3fb00932e332db9eab3b1f1765ca9cd6f3ec6c
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 13:21:13 +0000

    Move project

--- realtor-registry/observables/realtor-list/jest.config.ts
+++ realtor-registry/observables/realtor-list/jest.config.ts
@@ -1,11 +1,12 @@
 /* eslint-disable */
 export default {
   displayName: 'realtor-list-observable-in-realtor-registry',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/realtor-registry/realtor-list-observable',
+  coverageDirectory:
+    '../../../coverage/realtor-registry/observables/realtor-list',
 };
