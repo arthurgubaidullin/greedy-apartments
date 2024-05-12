7fc7d9d0cbd9f58e0ae48c198461ba365a19a1b0
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 16:15:56 +0000

    Fix repository

--- realtor-management/offer-repository/src/lib/offer-repository.ts
+++ realtor-management/offer-repository/src/lib/offer-repository.ts
@@ -1,27 +1,36 @@
-import * as RealtorDocument from '@ga/offer-document-in-realtor-management';
+import * as OfferDocument from '@ga/offer-document-in-realtor-management';
 import * as RealtorId from '@ga/realtor-id';
 import * as OfferId from '@ga/offer-id';
 
 const db: Map<
   RealtorId.T,
-  Map<OfferId.T, RealtorDocument.OfferDocument>
+  Map<OfferId.T, OfferDocument.OfferDocument>
 > = new Map();
 
+const getRealtorDB = (
+  realtorId: RealtorId.T
+): Map<OfferId.T, OfferDocument.OfferDocument> => {
+  let realtorDb = db.get(realtorId);
+
+  if (!realtorDb) {
+    realtorDb = new Map();
+    db.set(realtorId, realtorDb);
+  }
+
+  return realtorDb;
+};
+
 export const get = (realtorId: RealtorId.T) => {
+  const db = getRealtorDB(realtorId);
+
   return {
-    create: (document: RealtorDocument.OfferDocument): void => {
-      const id = RealtorDocument.getId(document);
-
-      const realtorDb = db.get(realtorId);
-
-      if (!realtorDb) {
-        db.set(realtorId, new Map([[id, document]]));
-        return;
+    create: (document: OfferDocument.OfferDocument): void => {
+      if (!db.has(document.id)) {
+        db.set(document.id, document);
       }
-
-      if (realtorDb.has(id)) {
-        realtorDb.set(id, document);
-      }
+    },
+    getAll: (): ReadonlyArray<OfferDocument.OfferDocument> => {
+      return Array.from(db.values());
     },
   };
 };
