ed87bebfce3403f6b367c9a71b2156a8721aa353
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 17:11:10 +0000

    Add type OfferStruct

--- realtor-management/offer-struct/src/lib/offet-struct.ts
+++ realtor-management/offer-struct/src/lib/offet-struct.ts
@@ -1,3 +1,14 @@
-export function offetStructInRealtorManagement(): string {
-  return 'offet-struct-in-realtor-management';
-}
+import * as OfferId from '@ga/offer-id';
+import * as RealtorId from '@ga/realtor-id';
+import * as t from 'io-ts';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface OfferStruct extends t.TypeOf<typeof OfferStruct> {}
+
+export const OfferStruct = t.readonly(
+  t.strict({
+    id: OfferId.codec,
+    realtorId: RealtorId.codec,
+    name: t.string,
+  })
+);
