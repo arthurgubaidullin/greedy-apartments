755379626aa805942201e44e1dc229eb964c906d
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 14:05:06 +0000

    Fix observable realtorList

--- realtor-registry/observables/realtor-list/src/lib/realtor-list-observable.ts
+++ realtor-registry/observables/realtor-list/src/lib/realtor-list-observable.ts
@@ -1,6 +1,11 @@
+import { getRealtorListApi } from '@ga/get-realtor-list-api-in-realtor-registry';
+import { realtorAdded } from '@ga/realtor-added-observable-in-realtor-registry';
+import * as RealtorStruct from '@ga/realtor-struct-in-realtor-registry';
 import * as _Eq from 'fp-ts/Eq';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
+import { constVoid, pipe } from 'fp-ts/function';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import {
   IObservableValue,
   action,
@@ -9,22 +14,20 @@ import {
   onBecomeObserved,
   onBecomeUnobserved,
 } from 'mobx';
-import * as RealtorStruct from '@ga/realtor-struct-in-realtor-registry';
-import { getRealtorListApi } from '@ga/get-realtor-list-api-in-realtor-registry';
-import { constVoid, pipe } from 'fp-ts/function';
-import { realtorAdded } from '@ga/realtor-added-observable-in-realtor-registry';
 
 type RealtorStructROArrayOption = O.Option<
-  ReadonlyArray<RealtorStruct.RealtorStruct>
+  RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>
 >;
 
 const _realtorList = observable.box<RealtorStructROArrayOption>(O.none, {
-  equals: O.getEq(RA.getEq(RealtorStruct.Eq)).equals,
+  equals: O.getEq(RNEA.getEq(RealtorStruct.Eq)).equals,
 } satisfies _Eq.Eq<RealtorStructROArrayOption>);
 
-const update = action(() => {
-  pipe(getRealtorListApi(), O.some, (list) => _realtorList.set(list));
-});
+const update = action((): void =>
+  pipe(getRealtorListApi(), O.fromPredicate(RA.isNonEmpty), (list) =>
+    _realtorList.set(list)
+  )
+);
 
 const subscribe = () =>
   autorun(() => pipe(realtorAdded.get(), O.fold(constVoid, update)));
