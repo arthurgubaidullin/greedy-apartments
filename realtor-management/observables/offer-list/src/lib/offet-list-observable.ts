ada581ad83ddf4247bcb74208a600ac5f569c585
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 17:53:38 +0000

    Fix observable offerList

--- realtor-management/observables/offer-list/src/lib/offet-list-observable.ts
+++ realtor-management/observables/offer-list/src/lib/offet-list-observable.ts
@@ -1,12 +1,35 @@
-import * as O from 'fp-ts/Option';
-import { IObservableValue, observable } from 'mobx';
+import { getOffetListApi } from '@ga/get-offet-list-api-in-realtor-management';
 import * as OfferStruct from '@ga/offet-struct-in-realtor-management';
-import { Eq } from 'fp-ts/Eq';
+import * as RealtorId from '@ga/realtor-id';
+import * as _Eq from 'fp-ts/Eq';
+import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
+import { pipe } from 'fp-ts/function';
+import { IObservableValue, observable, onBecomeObserved } from 'mobx';
 
-type T = O.Option<OfferStruct.OfferStruct>;
+type T = O.Option<RNEA.ReadonlyNonEmptyArray<OfferStruct.OfferStruct>>;
+
+const currentRealtorId = observable.box<O.Option<RealtorId.T>>(O.none, {
+  equals: O.getEq(RealtorId.Eq).equals,
+} satisfies _Eq.Eq<O.Option<RealtorId.T>>);
 
 const _offerList = observable.box<T>(O.none, {
-  equals: O.getEq(OfferStruct.Eq).equals,
-} satisfies Eq<T>);
+  equals: O.getEq(RNEA.getEq(OfferStruct.Eq)).equals,
+} satisfies _Eq.Eq<T>);
+
+const update = (): void => {
+  pipe(
+    currentRealtorId.get(),
+    O.map((realtorId) => ({ realtorId })),
+    O.map(getOffetListApi),
+    O.chain(O.fromPredicate(RA.isNonEmpty)),
+    (list) => _offerList.set(list)
+  );
+};
+
+onBecomeObserved(_offerList, () => {
+  update();
+});
 
 export const offerList: Pick<IObservableValue<T>, 'get'> = _offerList;
