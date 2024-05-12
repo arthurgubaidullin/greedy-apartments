dc58abe34dba3ecc5996f8edfecd8432bef91322
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-12 16:16:13 +0000

    Add function getOffetListApi

--- realtor-management/get-offer-list-api/src/lib/get-offet-list-api.ts
+++ realtor-management/get-offer-list-api/src/lib/get-offet-list-api.ts
@@ -1,3 +1,11 @@
-export function getOffetListApiInRealtorManagement(): string {
-  return 'get-offet-list-api-in-realtor-management';
-}
+import * as OfferDocument from '@ga/offer-document-in-realtor-management';
+import * as OfferRepository from '@ga/offer-repository-in-realtor-management';
+import * as RealtorId from '@ga/realtor-id';
+
+export const getOffetListApi = (
+  data: Readonly<{
+    realtorId: RealtorId.T;
+  }>
+): ReadonlyArray<OfferDocument.OfferDocument> => {
+  return OfferRepository.get(data.realtorId).getAll();
+};
