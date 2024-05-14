75fff59bfc8b20ee2fef479b5f8f975552ae35f5
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-14 07:39:53 +0000

    Fix observable

--- remote-offer-register/observables/offer-list/src/lib/offer-list-observable.ts
+++ remote-offer-register/observables/offer-list/src/lib/offer-list-observable.ts
@@ -1,13 +1,37 @@
 import * as OfferStruct from '@ga/offer-struct-in-remote-offer-register';
+import * as ServiceId from '@ga/service-id-in-remote-offer-register';
 import * as _Eq from 'fp-ts/Eq';
 import * as O from 'fp-ts/Option';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
-import { IObservableValue, observable } from 'mobx';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { IObservableValue, action, observable, onBecomeObserved } from 'mobx';
+import { getOfferListApi } from '@ga/get-offer-list-api-in-remote-offer-register';
+import { pipe } from 'fp-ts/function';
 
 type T = O.Option<RNEA.ReadonlyNonEmptyArray<OfferStruct.OfferStruct>>;
 
-const _offerList = observable.box<T>(O.none, {
-  equals: O.getEq(RNEA.getEq(OfferStruct.Eq)).equals,
-} satisfies _Eq.Eq<T>);
+const createOfferList = () =>
+  observable.box<T>(O.none, {
+    equals: O.getEq(RNEA.getEq(OfferStruct.Eq)).equals,
+  } satisfies _Eq.Eq<T>);
 
-export const offerList: Pick<IObservableValue<T>, 'get'> = _offerList;
+const update = (_offerList: IObservableValue<T>) =>
+  action((id: ServiceId.ServiceId): void => {
+    pipe(getOfferListApi(id), O.fromPredicate(RA.isNonEmpty), (list) =>
+      _offerList.set(list)
+    );
+  });
+
+export const getOfferList = (
+  id: ServiceId.ServiceId
+): Pick<IObservableValue<T>, 'get'> => {
+  const _offerList = createOfferList();
+
+  const _update = update(_offerList);
+
+  onBecomeObserved(_offerList, () => {
+    _update(id);
+  });
+
+  return _offerList;
+};
