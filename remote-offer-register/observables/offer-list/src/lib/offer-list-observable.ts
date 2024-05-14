1b761f6e2096360a34dc71e3b24fcfda4895a601
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-14 07:11:27 +0000

    Fix observable

--- remote-offer-register/observables/offer-list/src/lib/offer-list-observable.ts
+++ remote-offer-register/observables/offer-list/src/lib/offer-list-observable.ts
@@ -1,13 +1,13 @@
-import * as OfferDocument from '@ga/offer-document-in-remote-offer-register';
+import * as OfferStruct from '@ga/offer-struct-in-remote-offer-register';
+import * as _Eq from 'fp-ts/Eq';
 import * as O from 'fp-ts/Option';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
-import * as _Eq from 'fp-ts/Eq';
 import { IObservableValue, observable } from 'mobx';
 
-type T = O.Option<RNEA.ReadonlyNonEmptyArray<OfferDocument.OfferDocument>>;
+type T = O.Option<RNEA.ReadonlyNonEmptyArray<OfferStruct.OfferStruct>>;
 
 const _offerList = observable.box<T>(O.none, {
-  equals: O.getEq(RNEA.getEq(OfferDocument.Eq)).equals,
+  equals: O.getEq(RNEA.getEq(OfferStruct.Eq)).equals,
 } satisfies _Eq.Eq<T>);
 
 export const offerList: Pick<IObservableValue<T>, 'get'> = _offerList;
