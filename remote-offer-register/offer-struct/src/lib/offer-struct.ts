d19f2909c7357f6bec07dd9f00b10ab485c49233
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-14 07:09:38 +0000

    Add type OfferStruct

--- remote-offer-register/offer-struct/src/lib/offer-struct.ts
+++ remote-offer-register/offer-struct/src/lib/offer-struct.ts
@@ -1,3 +1,19 @@
-export function offerStructInRemoteOfferRegister(): string {
-  return 'offer-struct-in-remote-offer-register';
-}
+import * as OfferId from '@ga/offer-id-in-remote-offer-register';
+import * as _Eq from 'fp-ts/Eq';
+import * as S from 'fp-ts/string';
+import * as t from 'io-ts';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface OfferStruct extends t.TypeOf<typeof codec> {}
+
+export const codec = t.readonly(
+  t.strict({
+    id: OfferId.codec,
+    name: t.string,
+  })
+);
+
+export const Eq: _Eq.Eq<OfferStruct> = _Eq.struct({
+  id: OfferId.Eq,
+  name: S.Eq,
+});
