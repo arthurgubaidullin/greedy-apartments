dc63fdbf49e352448d9fe788da566dfb142b0072
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-15 05:08:16 +0000

    Add function getOfferAdded

--- remote-offer-register/observables/offer-added/src/lib/offer-added-observable.ts
+++ remote-offer-register/observables/offer-added/src/lib/offer-added-observable.ts
@@ -1,3 +1,19 @@
-export function offerAddedObservableInRemoteOfferRegister(): string {
-  return 'offer-added-observable-in-remote-offer-register';
-}
+import * as OfferStruct from '@ga/offer-struct-in-remote-offer-register';
+import * as _Eq from 'fp-ts/Eq';
+import * as O from 'fp-ts/Option';
+import { IObservableValue, action, observable } from 'mobx';
+
+type T = O.Option<OfferStruct.OfferStruct>;
+
+const _getOfferAdded = () =>
+  observable.box<T>(O.none, {
+    equals: O.getEq(OfferStruct.Eq).equals,
+  } satisfies _Eq.Eq<T>);
+
+export const getOfferAdded = (): Pick<IObservableValue<T>, 'get'> =>
+  _getOfferAdded();
+
+export const publish = (store: IObservableValue<T>) =>
+  action((data: OfferStruct.OfferStruct) => {
+    store.set(O.some(data));
+  });
