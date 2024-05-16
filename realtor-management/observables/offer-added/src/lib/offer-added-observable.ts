906fd201aa894d9c5dde8cea36f45eb8fc2542c8
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-16 17:20:55 +0000

    Add observable OfferAdded

--- realtor-management/observables/offer-added/src/lib/offer-added-observable.ts
+++ realtor-management/observables/offer-added/src/lib/offer-added-observable.ts
@@ -1,3 +1,26 @@
-export function offerAddedObservableInRealtorManagement(): string {
-  return 'offer-added-observable-in-realtor-management';
-}
+import * as OfferStruct from '@ga/offet-struct-in-realtor-management';
+import * as _Eq from 'fp-ts/Eq';
+import * as O from 'fp-ts/Option';
+import { IObservableValue, action, observable } from 'mobx';
+
+type T = O.Option<OfferStruct.OfferStruct>;
+
+const _getOfferAdded = () =>
+  observable.box<T>(O.none, {
+    equals: O.getEq(OfferStruct.Eq).equals,
+  } satisfies _Eq.Eq<T>);
+
+export const publish = (store: IObservableValue<T>) =>
+  action((data: OfferStruct.OfferStruct) => {
+    store.set(O.some(data));
+  });
+
+export const get = (): Pick<IObservableValue<T>, 'get'> & {
+  publish: (data: OfferStruct.OfferStruct) => void;
+} => {
+  const store = _getOfferAdded();
+  return {
+    get: () => store.get(),
+    publish: publish(store),
+  };
+};
