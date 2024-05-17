6d2da4b3fe113d013d9723517119540932e5fedd
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-17 06:04:08 +0000

    Add function changeServiceApi

--- remote-offer-register/change-service-api/src/lib/change-service-api.ts
+++ remote-offer-register/change-service-api/src/lib/change-service-api.ts
@@ -1,3 +1,18 @@
-export function changeServiceApiInRemoteOfferRegister(): string {
-  return 'change-service-api-in-remote-offer-register';
+import * as ServiceId from '@ga/service-id-in-remote-offer-register';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { NonEmptyString } from 'io-ts-types';
+
+interface ChangeService {
+  readonly change: (value: O.Option<ServiceId.ServiceId>) => void;
 }
+
+export const changeServiceApi =
+  (currentService: ChangeService) =>
+  (serviceId: NonEmptyString): void => {
+    return pipe(
+      ServiceId.fromNonEmptyString(serviceId),
+      O.some,
+      currentService.change
+    );
+  };
