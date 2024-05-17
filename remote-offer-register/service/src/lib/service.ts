be00e96ef4ec66945cd9563d0050e7e5db2afdd5
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-17 06:12:02 +0000

    Fix service

--- remote-offer-register/service/src/lib/service.ts
+++ remote-offer-register/service/src/lib/service.ts
@@ -1,12 +1,19 @@
+import { changeServiceApi } from '@ga/change-service-api-in-remote-offer-register';
+import * as CurrentService from '@ga/current-service-observable-in-remote-offer-register';
+import * as OfferAdded from '@ga/offer-added-observable-in-remote-offer-register';
 import * as OfferDocument from '@ga/offer-document-in-remote-offer-register';
 import * as OfferList from '@ga/offer-list-observable-in-remote-offer-register';
-import * as OfferAdded from '@ga/offer-added-observable-in-remote-offer-register';
 import * as OfferStruct from '@ga/offer-struct-in-remote-offer-register';
 import { publishOfferApi } from '@ga/publish-offer-api-in-remote-offer-register';
 import * as ServiceId from '@ga/service-id-in-remote-offer-register';
 import * as O from 'fp-ts/Option';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
+import { NonEmptyString } from 'io-ts-types';
 import { IObservableValue } from 'mobx';
+
+interface ChangeService {
+  readonly changeService: (value: NonEmptyString) => void;
+}
 
 interface PublishOffer {
   readonly publishOffer: (
@@ -23,15 +30,21 @@ interface OfferListObservable {
   >;
 }
 
-type Service = PublishOffer & OfferListObservable;
+type Service = PublishOffer & OfferListObservable & ChangeService;
 
 export const get = (id: ServiceId.ServiceId) => {
+  const currentService = CurrentService.create();
+  const changeService = changeServiceApi(currentService);
+
   const offerAdded = OfferAdded.get();
+
   const publishOffer = publishOfferApi(id)(offerAdded.publish);
+
   const offerList = OfferList.getOfferList(id)(offerAdded);
 
   return {
     offerList,
     publishOffer,
-  } satisfies Service;
+    changeService,
+  } as const satisfies Service;
 };
