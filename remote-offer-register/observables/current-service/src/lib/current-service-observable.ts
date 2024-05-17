c05eb530c99143172fc27207357c567a64d8898a
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-17 05:39:27 +0000

    Add observable CurrentService

--- remote-offer-register/observables/current-service/src/lib/current-service-observable.ts
+++ remote-offer-register/observables/current-service/src/lib/current-service-observable.ts
@@ -1,3 +1,11 @@
-export function currentServiceObservableInRemoteOfferRegister(): string {
-  return 'current-service-observable-in-remote-offer-register';
-}
+import * as ServiceId from '@ga/service-id-in-remote-offer-register';
+import * as _Eq from 'fp-ts/Eq';
+import * as O from 'fp-ts/Option';
+import { IObservableValue, observable } from 'mobx';
+
+type ServiceIdOption = O.Option<ServiceId.ServiceId>;
+
+export const create = (): IObservableValue<ServiceIdOption> =>
+  observable.box<ServiceIdOption>(O.none, {
+    equals: O.getEq(ServiceId.Eq).equals,
+  } satisfies _Eq.Eq<ServiceIdOption>);
