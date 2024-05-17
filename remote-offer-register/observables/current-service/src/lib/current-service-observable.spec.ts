1060127d96a374468107d6521f4931e0616f457c
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-17 05:33:15 +0000

    Add project current-service-observable-in-remote-offer-register

---
+++ remote-offer-register/observables/current-service/src/lib/current-service-observable.spec.ts
@@ -0,0 +1,5 @@
+describe('current-service-observable', () => {
+  it('should work', () => {
+    expect(1).toEqual(1);
+  });
+});
---
+++ remote-offer-register/observables/current-service/src/lib/current-service-observable.ts
@@ -0,0 +1,3 @@
+export function currentServiceObservableInRemoteOfferRegister(): string {
+  return 'current-service-observable-in-remote-offer-register';
+}
