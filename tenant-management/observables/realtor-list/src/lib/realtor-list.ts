638d5fee8a96287449463ea86c8603184b93d45d
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 11:19:42 +0000

    Add observable RealtorList

--- tenant-management/observables/realtor-list/src/lib/realtor-list.ts
+++ tenant-management/observables/realtor-list/src/lib/realtor-list.ts
@@ -1,3 +1,19 @@
-export function realtorListInTenantManagement(): string {
-  return 'realtor-list-in-tenant-management';
-}
+import * as O from 'fp-ts/Option';
+import { IObservableValue, observable } from 'mobx';
+import * as RealtorStruct from '@ga/realtor-struct-in-tenant-management';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
+import * as _Eq from 'fp-ts/Eq';
+
+export const _realtorList = () =>
+  observable.box<
+    O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>
+  >(O.none, {
+    equals: O.getEq(RNEA.getEq(RealtorStruct.Eq)).equals,
+  } satisfies _Eq.Eq<O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>>);
+
+export const realtorList = (): Pick<
+  IObservableValue<
+    O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>
+  >,
+  'get'
+> => _realtorList();
