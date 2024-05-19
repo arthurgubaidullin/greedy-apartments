374b3070efaeb6297a455d949ce516de88d914dc
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 13:16:05 +0000

    Fix react component

--- web-app/src/tenant-management/tenant-management.tsx
+++ web-app/src/tenant-management/tenant-management.tsx
@@ -1,15 +1,52 @@
+import * as Service from '@ga/service-in-tenant-management';
+import * as O from 'fp-ts/Option';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
+import { pipe } from 'fp-ts/function';
+
+const service = Service.get();
+
 export const RealtorList = () => {
+  const list = pipe(
+    service.realtorList.get(),
+    O.fold(
+      () => <p>No realtors.</p>,
+      (list) =>
+        pipe(
+          list,
+          RNEA.map((realtor) => <li key={realtor.id}>{realtor.name}</li>),
+          (list) => <ul>{list}</ul>
+        )
+    )
+  );
+
   return (
     <div>
       <h2>Realtor list</h2>
+
+      {list}
     </div>
   );
 };
 
 export const OfferList = () => {
+  const list = pipe(
+    service.offerList.get(),
+    O.fold(
+      () => <p>No offers.</p>,
+      (list) =>
+        pipe(
+          list,
+          RNEA.map((offer) => <li key={offer.id}>{offer.name}</li>),
+          (list) => <ul>{list}</ul>
+        )
+    )
+  );
+
   return (
     <div>
-      <h2>Realtor list</h2>
+      <h2>Offer list</h2>
+
+      {list}
     </div>
   );
 };
