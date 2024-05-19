9f5b7241f6c0ad6b0623073653ccc8eb0b5a408f
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 11:21:25 +0000

    Rename

--- tenant-management/observables/realtor-list/src/lib/realtor-list.ts
+++ tenant-management/observables/realtor-list/src/lib/realtor-list.ts
@@ -4,16 +4,16 @@ import * as RealtorStruct from '@ga/realtor-struct-in-tenant-management';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import * as _Eq from 'fp-ts/Eq';
 
-export const _realtorList = () =>
+const realtorList = () =>
   observable.box<
     O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>
   >(O.none, {
     equals: O.getEq(RNEA.getEq(RealtorStruct.Eq)).equals,
   } satisfies _Eq.Eq<O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>>);
 
-export const realtorList = (): Pick<
+export const get = (): Pick<
   IObservableValue<
     O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>
   >,
   'get'
-> => _realtorList();
+> => realtorList();
