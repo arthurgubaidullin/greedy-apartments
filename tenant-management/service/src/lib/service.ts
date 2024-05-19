c07876de8d0856675f87274a3c03bb9c3308bddb
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 18:58:11 +0000

    Fix service

--- tenant-management/service/src/lib/service.ts
+++ tenant-management/service/src/lib/service.ts
@@ -1,8 +1,10 @@
 import * as OfferList from '@ga/offer-list-in-tenant-management';
+import * as RealtorList from '@ga/realtor-list-in-tenant-management';
 import { updateRealtor } from '@ga/update-realtor-api-in-tenant-management';
 
 export const get = () => {
   return {
+    realtorList: RealtorList.get(),
     offerList: OfferList.get(),
     updateRealtor: updateRealtor,
   } as const;
