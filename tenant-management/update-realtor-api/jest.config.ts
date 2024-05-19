543acb678fc8b1bb9ace84cbc31a4e995c4cfdfb
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 13:44:38 +0000

    Move & rename project
    update-realtor-document-in-tenant-management → update-realtor-api-in-tenant-management

--- tenant-management/update-realtor-api/jest.config.ts
+++ tenant-management/update-realtor-api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'update-realtor-document-in-tenant-management',
+  displayName: 'update-realtor-api-in-tenant-management',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/tenant-management/update-realtor-document',
+  coverageDirectory: '../../coverage/tenant-management/update-realtor-api',
 };
