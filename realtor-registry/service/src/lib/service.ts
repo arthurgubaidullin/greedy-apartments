56d1ad5c9a2a7701baa1e2c15314940bb0365e79
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 18:48:31 +0000

    Add observable to service

--- realtor-registry/service/src/lib/service.ts
+++ realtor-registry/service/src/lib/service.ts
@@ -1,9 +1,11 @@
 import { registerRealtorApi } from '@ga/register-realtor-api-in-registry';
 import { realtorList } from '@ga/realtor-list-observable-in-realtor-registry';
+import { realtorAdded } from '@ga/realtor-added-observable-in-realtor-registry';
 
 export const get = () => {
   return {
     registerRealtor: registerRealtorApi,
     realtorList,
+    realtorAdded
   } as const;
 };
