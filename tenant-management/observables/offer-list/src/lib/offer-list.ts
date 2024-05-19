301238f71adff3219494f9d37cb053231a0ae09b
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 13:09:21 +0000

    Fix observable OfferList

--- tenant-management/observables/offer-list/src/lib/offer-list.ts
+++ tenant-management/observables/offer-list/src/lib/offer-list.ts
@@ -1,11 +1,17 @@
-import * as O from 'fp-ts/Option';
-import { IObservableValue, observable } from 'mobx';
 import * as OfferStruct from '@ga/offer-struct-in-tenant-management';
+import * as _Eq from 'fp-ts/Eq';
+import * as O from 'fp-ts/Option';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
+import { IObservableValue, observable } from 'mobx';
 
 const create = () =>
   observable.box<O.Option<RNEA.ReadonlyNonEmptyArray<OfferStruct.OfferStruct>>>(
-    O.none
+    O.none,
+    {
+      equals: O.getEq(RNEA.getEq(OfferStruct.Eq)).equals,
+    } satisfies _Eq.Eq<
+      O.Option<RNEA.ReadonlyNonEmptyArray<OfferStruct.OfferStruct>>
+    >
   );
 
 export const get = (): IObservableValue<
