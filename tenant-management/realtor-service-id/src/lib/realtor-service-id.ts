954476071873ccba4883d16d2309dbb9346bacbd
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 16:11:45 +0000

    Add type RealtorServiceId

--- tenant-management/realtor-service-id/src/lib/realtor-service-id.ts
+++ tenant-management/realtor-service-id/src/lib/realtor-service-id.ts
@@ -1,3 +1,21 @@
-export function realtorServiceIdInTenantManagement(): string {
-  return 'realtor-service-id-in-tenant-management';
-}
+import * as _Eq from 'fp-ts/Eq';
+import * as S from 'fp-ts/string';
+import { NonEmptyString, fromNewtype } from 'io-ts-types';
+import * as Newtype from 'newtype-ts';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface RealtorServiceId
+  extends Newtype.Newtype<
+    { readonly RealtorServiceId: unique symbol },
+    NonEmptyString
+  > {}
+
+export const codec = fromNewtype<RealtorServiceId>(NonEmptyString);
+
+export const Eq: _Eq.Eq<RealtorServiceId> = Newtype.getEq<RealtorServiceId>(
+  S.Eq
+);
+
+const iso = Newtype.iso<RealtorServiceId>();
+
+export const fromNonEmptyString = (s: NonEmptyString) => iso.wrap(s);
