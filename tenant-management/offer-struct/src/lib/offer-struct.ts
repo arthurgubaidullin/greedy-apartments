ef4dcad437d688c380ed0f008bd8399d9d27699f
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 12:55:10 +0000

    Add type OfferStruct

--- tenant-management/offer-struct/src/lib/offer-struct.ts
+++ tenant-management/offer-struct/src/lib/offer-struct.ts
@@ -1,3 +1,10 @@
-export function offerStructInTenantManagement(): string {
-  return 'offer-struct-in-tenant-management';
-}
+import * as t from 'io-ts';
+import { NonEmptyString } from 'io-ts-types';
+
+const OfferStruct = t.strict({
+  id: NonEmptyString,
+  realtorId: NonEmptyString,
+  name: t.string,
+});
+
+export type OfferStruct = t.TypeOf<typeof OfferStruct>;
