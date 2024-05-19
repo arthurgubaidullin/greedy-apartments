d5c1c3f75155e97bde7cc64bbc1634568dba5764
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 10:54:56 +0000

    Fix react component

--- web-app/src/tenant-management/tenant-management.tsx
+++ web-app/src/tenant-management/tenant-management.tsx
@@ -1,5 +1,29 @@
+export const RealtorList = () => {
+  return (
+    <div>
+      <h2>Realtor list</h2>
+    </div>
+  );
+};
+
+export const OfferList = () => {
+  return (
+    <div>
+      <h2>Realtor list</h2>
+    </div>
+  );
+};
+
 export function TenantManagement() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Tenant Management</h1>
+
+      <RealtorList />
+
+      <OfferList />
+    </div>
+  );
 }
 
 export default TenantManagement;
