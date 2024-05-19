2956be974eeb2581dfab3f8caa44dea4bebbb3b2
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 13:02:56 +0000

    Add observable OfferList

--- tenant-management/observables/offer-list/src/lib/offer-list.ts
+++ tenant-management/observables/offer-list/src/lib/offer-list.ts
@@ -1,3 +1,13 @@
-export function offerListInTenantManagement(): string {
-  return 'offer-list-in-tenant-management';
-}
+import * as O from 'fp-ts/Option';
+import { IObservableValue, observable } from 'mobx';
+import * as OfferStruct from '@ga/offer-struct-in-tenant-management';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
+
+const create = () =>
+  observable.box<O.Option<RNEA.ReadonlyNonEmptyArray<OfferStruct.OfferStruct>>>(
+    O.none
+  );
+
+export const get = (): IObservableValue<
+  O.Option<RNEA.ReadonlyNonEmptyArray<OfferStruct.OfferStruct>>
+> => create();
