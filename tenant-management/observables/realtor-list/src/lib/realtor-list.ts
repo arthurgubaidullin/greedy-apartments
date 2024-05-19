549621aca0e882c1c73383937002cd9970df23f7
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 18:56:55 +0000

    Fix observable

--- tenant-management/observables/realtor-list/src/lib/realtor-list.ts
+++ tenant-management/observables/realtor-list/src/lib/realtor-list.ts
@@ -2,8 +2,7 @@ import * as RealtorStruct from '@ga/realtor-struct-in-tenant-management';
 import * as _Eq from 'fp-ts/Eq';
 import * as O from 'fp-ts/Option';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
-import { pipe } from 'fp-ts/function';
-import { IObservableValue, autorun, observable } from 'mobx';
+import { IObservableValue, observable } from 'mobx';
 
 const create = () =>
   observable.box<
@@ -12,22 +11,13 @@ const create = () =>
     equals: O.getEq(RNEA.getEq(RealtorStruct.Eq)).equals,
   } satisfies _Eq.Eq<O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>>);
 
-export const get = (
-  remoteRealtorList: Pick<
-    IObservableValue<
-      O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>
-    >,
-    'get'
-  >
-): Pick<
+export const get = (): Pick<
   IObservableValue<
     O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>
   >,
   'get'
 > => {
   const realtorList = create();
-
-  autorun(() => pipe(remoteRealtorList.get(), (data) => realtorList.set(data)));
 
   return realtorList;
 };
