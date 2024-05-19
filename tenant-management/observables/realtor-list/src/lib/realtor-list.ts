ee58e4f2e33129e818a797c66541b6b82ffb6791
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 13:55:47 +0000

    Fix observables

--- tenant-management/observables/realtor-list/src/lib/realtor-list.ts
+++ tenant-management/observables/realtor-list/src/lib/realtor-list.ts
@@ -1,19 +1,33 @@
-import * as O from 'fp-ts/Option';
-import { IObservableValue, observable } from 'mobx';
 import * as RealtorStruct from '@ga/realtor-struct-in-tenant-management';
-import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import * as _Eq from 'fp-ts/Eq';
+import * as O from 'fp-ts/Option';
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
+import { pipe } from 'fp-ts/function';
+import { IObservableValue, autorun, observable } from 'mobx';
 
-const realtorList = () =>
+const create = () =>
   observable.box<
     O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>
   >(O.none, {
     equals: O.getEq(RNEA.getEq(RealtorStruct.Eq)).equals,
   } satisfies _Eq.Eq<O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>>);
 
-export const get = (): Pick<
+export const get = (
+  remoteRealtorList: Pick<
+    IObservableValue<
+      O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>
+    >,
+    'get'
+  >
+): Pick<
   IObservableValue<
     O.Option<RNEA.ReadonlyNonEmptyArray<RealtorStruct.RealtorStruct>>
   >,
   'get'
-> => realtorList();
+> => {
+  const realtorList = create();
+
+  autorun(() => pipe(remoteRealtorList.get(), (data) => realtorList.set(data)));
+
+  return realtorList;
+};
--- tenant-management/service/src/lib/service.ts
+++ tenant-management/service/src/lib/service.ts
@@ -1,9 +1,12 @@
-import * as RealtorList from '@ga/realtor-list-in-tenant-management';
 import * as OfferList from '@ga/offer-list-in-tenant-management';
+import * as RealtorList from '@ga/realtor-list-in-tenant-management';
+import * as RealtorRegistry from '@ga/service-in-realtor-registry';
 
 export const get = () => {
+  const registry = RealtorRegistry.get();
+
   return {
-    realtorList: RealtorList.get(),
+    realtorList: RealtorList.get(registry.realtorList),
     offerList: OfferList.get(),
   } as const;
 };
