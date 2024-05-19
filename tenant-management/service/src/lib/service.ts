4f45286ff7ec9aa49cb64e3f3a0d5b52b73f8469
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-19 13:05:22 +0000

    Add observable OfferList to service

--- tenant-management/service/src/lib/service.ts
+++ tenant-management/service/src/lib/service.ts
@@ -1,7 +1,9 @@
 import * as RealtorList from '@ga/realtor-list-in-tenant-management';
+import * as OfferList from '@ga/offer-list-in-tenant-management';
 
 export const get = () => {
   return {
     realtorList: RealtorList.get(),
+    offerList: OfferList.get(),
   } as const;
 };
