a3c6e9c2b57e311cc12db5192191d4f8fcebfc13
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-20 07:26:56 +0000

    Add logs

--- tenant-space/realtors/update-realtor-api/src/lib/update-realtor-document.ts
+++ tenant-space/realtors/update-realtor-api/src/lib/update-realtor-document.ts
@@ -1,21 +1,27 @@
-import * as RealtorRepository from '@ga/realtor-repository-in-tenant-space';
 import * as RealtorDocument from '@ga/realtor-document-in-tenant-space';
+import * as RealtorRepository from '@ga/realtor-repository-in-tenant-space';
 import * as RealtorStruct from '@ga/realtor-struct-in-tenant-space';
+import * as Console from 'fp-ts/Console';
+import * as IO from 'fp-ts/IO';
+import * as IOO from 'fp-ts/IOOption';
 import { pipe } from 'fp-ts/function';
-import * as O from 'fp-ts/Option';
 
 const repository = RealtorRepository.get();
 
 const updateRealtorDocument = (
   document: RealtorDocument.RealtorDocument
-): void =>
+): IO.IO<void> =>
   pipe(
     repository.get(document.id),
-    O.fold(
-      () => repository.create(document),
-      () => repository.update(document)
+    IOO.fromOption,
+    IOO.fold(
+      () => IO.of(repository.create(document)),
+      () => IO.of(repository.update(document))
+    ),
+    IO.chainFirst(() =>
+      Console.info(`Realtor document created in tenant space.`)
     )
   );
 
-export const updateRealtor = (data: RealtorStruct.RealtorStruct) =>
-  pipe(data, RealtorDocument.fromSimplified, updateRealtorDocument);
+export const updateRealtor = (data: RealtorStruct.RealtorStruct): void =>
+  pipe(data, RealtorDocument.fromSimplified, updateRealtorDocument, (f) => f());
