6ac5c98af6acb460e562a253408bc2bfcd3cbdc7
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-20 05:39:47 +0000

    Fix type

--- realtor-registry/realtor-struct/src/lib/realtor-struct.ts
+++ realtor-registry/realtor-struct/src/lib/realtor-struct.ts
@@ -1,15 +1,18 @@
 import * as _Eq from 'fp-ts/Eq';
+import * as S from 'fp-ts/string';
 import * as t from 'io-ts';
 import { NonEmptyString } from 'io-ts-types';
 
 const RealtorStruct = t.strict({
   id: NonEmptyString,
   name: t.string,
+  serviceId: NonEmptyString,
 });
 
 export type RealtorStruct = t.TypeOf<typeof RealtorStruct>;
 
 export const Eq: _Eq.Eq<RealtorStruct> = _Eq.struct({
-  id: _Eq.eqStrict,
-  name: _Eq.eqStrict,
+  id: S.Eq,
+  name: S.Eq,
+  serviceId: S.Eq,
 });
