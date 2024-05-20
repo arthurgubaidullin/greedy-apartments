a45f3fc17a4d6714a6a776cf50adea7fa1ad492d
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-20 06:19:01 +0000

    Move project

--- tenant-space/realtors/realtor-list-observable/jest.config.ts
+++ tenant-space/realtors/realtor-list-observable/jest.config.ts
@@ -7,5 +7,6 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/tenant-space/observables/realtor-list',
+  coverageDirectory:
+    '../../../coverage/tenant-space/realtors/realtor-list-observable',
 };
