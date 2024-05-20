d16fa29e2499b8bb4c42c5df8829541c81c3535f
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-20 06:13:53 +0000

    Move project

--- tenant-space/realtors/realtor-struct/jest.config.ts
+++ tenant-space/realtors/realtor-struct/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'realtor-struct-in-tenant-space',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/tenant-space/realtor-struct',
+  coverageDirectory: '../../../coverage/tenant-space/realtors/realtor-struct',
 };
