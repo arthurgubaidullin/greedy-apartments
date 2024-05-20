773935ca72f4f7a338d7dc9f9060bf20e9d71b01
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-20 05:28:44 +0000

    Add function subscribeToRealtorRegisterApi

--- tenant-management/subscribe-to-realtor-register-api/src/lib/subscribe-to-realtor-register-api.ts
+++ tenant-management/subscribe-to-realtor-register-api/src/lib/subscribe-to-realtor-register-api.ts
@@ -1,3 +1,12 @@
-export function subscribeToRealtorRegisterApiIdInTenantManagement(): string {
-  return 'subscribe-to-realtor-register-api-in-tenant-management';
-}
+import { RealtorStruct } from '@ga/realtor-struct-in-tenant-management';
+import { updateRealtor } from '@ga/update-realtor-api-in-tenant-management';
+import * as O from 'fp-ts/Option';
+import { constVoid, pipe } from 'fp-ts/function';
+import { IObservableValue, autorun } from 'mobx';
+
+export const subscribeToRealtorRegisterApi = (
+  realtorAddedToRegistry: Pick<IObservableValue<O.Option<RealtorStruct>>, 'get'>
+) =>
+  autorun(() =>
+    pipe(realtorAddedToRegistry.get(), O.fold(constVoid, updateRealtor))
+  );
