753e726a8218b63648b9de31ddb2ea88b6973f23
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-20 06:13:00 +0000

    Move project

--- tenant-space/realtors/realtor-document/jest.config.ts
+++ tenant-space/realtors/realtor-document/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'realtor-document-in-tenant-space',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/tenant-space/realtor-document',
+  coverageDirectory: '../../../coverage/tenant-space/realtors/realtor-document',
 };
