535ba977c17104e66f0e953e3034c94b8a883e36
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-20 07:17:12 +0000

    Add function getRealtorListApi

--- tenant-space/realtors/get-realtor-list-api/src/lib/get-realtor-list-api.ts
+++ tenant-space/realtors/get-realtor-list-api/src/lib/get-realtor-list-api.ts
@@ -1,3 +1,14 @@
-export function getRealtorListApiInTenantSpace(): string {
-  return 'get-realtor-list-api-in-tenant-space';
-}
+import * as RealtorDocument from '@ga/realtor-document-in-tenant-space';
+import * as RealtorRepository from '@ga/realtor-repository-in-tenant-space';
+import * as RealtorStruct from '@ga/realtor-struct-in-tenant-space';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+
+const repository = RealtorRepository.get();
+
+const getRealtorDocumentList =
+  (): ReadonlyArray<RealtorDocument.RealtorDocument> => repository.getList();
+
+export const getRealtorListApi =
+  (): ReadonlyArray<RealtorStruct.RealtorStruct> =>
+    pipe(getRealtorDocumentList(), RA.map(RealtorDocument.toJSON));
