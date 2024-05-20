51ad9430dd5d339af2b10a104f1bb7976cc8acb1
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-20 06:23:47 +0000

    Move project

--- tenant-space/offers/offer-repository/jest.config.ts
+++ tenant-space/offers/offer-repository/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'offer-repository-in-tenant-space',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/tenant-space/offer-repository',
+  coverageDirectory: '../../../coverage/tenant-space/offers/offer-repository',
 };
