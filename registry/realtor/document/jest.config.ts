1b3c530b55a188904cd7bd05e88284fe41f0df88
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-25 19:59:49 +0000

    Move project

--- registry/realtor/document/jest.config.ts
+++ registry/realtor/document/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'realtor-document-in-registry',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/registry/realtor-document',
+  coverageDirectory: '../../../coverage/registry/realtor/document',
 };
