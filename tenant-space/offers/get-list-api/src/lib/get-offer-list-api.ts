98ac550681a3ed04a565c0a6981e7f8ce89a2470
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-25 13:42:59 +0000

    Add function

--- tenant-space/offers/get-list-api/src/lib/get-offer-list-api.ts
+++ tenant-space/offers/get-list-api/src/lib/get-offer-list-api.ts
@@ -1,3 +1,13 @@
-export function getOfferListApiInTenantSpace(): string {
-  return 'get-offer-list-api-in-tenant-space';
-}
+import * as OfferDocument from '@ga/offer-document-in-tenant-space';
+import * as OfferRepository from '@ga/offer-repository-in-tenant-space';
+import * as OfferStruct from '@ga/offer-struct-in-tenant-space';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { pipe } from 'fp-ts/function';
+
+const repository = OfferRepository.get();
+
+const getOfferDocumentList = (): ReadonlyArray<OfferDocument.OfferDocument> =>
+  repository.getList();
+
+export const getOfferListApi = (): ReadonlyArray<OfferStruct.OfferStruct> =>
+  pipe(getOfferDocumentList(), RA.map(OfferDocument.toJSON));
