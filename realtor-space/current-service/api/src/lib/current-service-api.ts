9683f5e2b36edb507f445eadc24b61b681b2d5e0
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-26 09:51:57 +0000

    Add api

--- realtor-space/current-service/api/src/lib/current-service-api.ts
+++ realtor-space/current-service/api/src/lib/current-service-api.ts
@@ -1,3 +1,23 @@
-export function currentServiceApiInRealtorSpace(): string {
-  return 'current-service-api-in-realtor-space';
+import * as _CurrentServiceIdObservable from '@ga/current-service-id-observable-in-realtor-space';
+import { ReadonlyObservable } from '@ga/readonly-observable';
+import * as O from 'fp-ts/Option';
+import { NonEmptyString } from 'io-ts-types';
+
+interface CurrentServiceIdObservable {
+  readonly currentServiceId: ReadonlyObservable<O.Option<NonEmptyString>>;
 }
+
+interface ConnectToService {
+  readonly connectToService: (serviceId: NonEmptyString) => void;
+}
+
+export const get = (): CurrentServiceIdObservable & ConnectToService => {
+  const currentServiceIdStore = _CurrentServiceIdObservable.get();
+
+  return {
+    currentServiceId: currentServiceIdStore,
+    connectToService: _CurrentServiceIdObservable.connect(
+      currentServiceIdStore
+    ),
+  };
+};
