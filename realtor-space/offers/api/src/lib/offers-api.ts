fc06d867b7459f790f62f1bc97b044ac89ed411e
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-26 13:48:35 +0000

    Move & rename project

--- realtor-space/offers/api/src/lib/offers-api.ts
+++ realtor-space/offers/api/src/lib/offers-api.ts
@@ -1,4 +1,4 @@
-import * as OfferAdded from '@ga/offer-added-observable-in-realtor-space';
+import * as OfferAdded from '@ga/offer-added-store-in-realtor-space';
 import * as OfferList from '@ga/offer-list-store-in-realtor-space';
 import { createOfferApi } from './create-offer-api';
 import { getOfferListApi } from './get-offer-list-api';
--- realtor-space/offers/stores/offer-added/jest.config.ts
+++ realtor-space/offers/stores/offer-added/jest.config.ts
@@ -1,11 +1,12 @@
 /* eslint-disable */
 export default {
-  displayName: 'offer-added-observable-in-realtor-space',
-  preset: '../../../jest.preset.js',
+  displayName: 'offer-added-store-in-realtor-space',
+  preset: '../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/realtor-space/observables/offer-added',
+  coverageDirectory:
+    '../../../../coverage/realtor-space/offers/stores/offer-added',
 };
