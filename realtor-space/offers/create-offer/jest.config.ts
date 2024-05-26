0cfc34bf0ca3d4c1d0b43c70ced6b30ab7873b72
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-26 12:32:22 +0000

    Move project

--- realtor-space/offers/create-offer/jest.config.ts
+++ realtor-space/offers/create-offer/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'create-offer-in-realtor-space',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/realtor-space/create-offer',
+  coverageDirectory: '../../../coverage/realtor-space/offers/create-offer',
 };
