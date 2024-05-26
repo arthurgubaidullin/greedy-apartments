7e31b2e4bbffc02d82446b7510490442d64831a3
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-26 15:56:35 +0000

    Add store

--- offer-space/services/stores/service-updated/src/lib/service-updated-store.ts
+++ offer-space/services/stores/service-updated/src/lib/service-updated-store.ts
@@ -1,3 +1,11 @@
-export function serviceUpdatedStoreInOfferSpace(): string {
-  return 'service-updated-store-in-offer-space';
-}
+import * as ServiceId from '@ga/service-id-in-offer-space';
+import * as _Eq from 'fp-ts/Eq';
+import * as O from 'fp-ts/Option';
+import { IObservableValue, observable } from 'mobx';
+
+type ServiceIdOption = O.Option<ServiceId.ServiceId>;
+
+export const create = (): IObservableValue<ServiceIdOption> =>
+  observable.box<ServiceIdOption>(O.none, {
+    equals: O.getEq(ServiceId.Eq).equals,
+  } satisfies _Eq.Eq<ServiceIdOption>);
