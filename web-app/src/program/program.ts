1acb7bc7c79eed68da34e53c2f1627476958942e
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-26 17:37:19 +0000

    Fix program

--- web-app/src/program/program.ts
+++ web-app/src/program/program.ts
@@ -1,11 +1,8 @@
 import * as RealtorSpace from '@ga/api-in-realtor-space';
-import * as RemoteOfferService from '@ga/api-in-offer-space';
 import * as TenantSpaceService from '@ga/public-api-in-tenant-space';
 import * as RealtorRegistry from '@ga/api-in-registry';
 
 export const realtorSpace = RealtorSpace.get();
-
-export const remoteOffers = RemoteOfferService.get();
 
 export const realtorRegistry = RealtorRegistry.get();
 
