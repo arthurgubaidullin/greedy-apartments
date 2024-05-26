76aae9da070a907bf1e7fe9f44cefd102bfe3e95
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-26 09:51:31 +0000

    Add observable

--- realtor-space/observables/current-service-id/src/lib/current-service-id-observable.ts
+++ realtor-space/observables/current-service-id/src/lib/current-service-id-observable.ts
@@ -1,3 +1,24 @@
-export function currentServiceIdObservableInRealtorSpace(): string {
-  return 'current-service-id-observable-in-realtor-space';
-}
+import { Eq } from 'fp-ts/Eq';
+import * as O from 'fp-ts/Option';
+import * as S from 'fp-ts/string';
+import { NonEmptyString } from 'io-ts-types';
+import { observable } from 'mobx';
+import { constVoid, pipe } from 'fp-ts/function';
+import { IObservableValue, action } from 'mobx';
+
+export type CurrentServiceIdOption = O.Option<NonEmptyString>;
+
+export const get = () =>
+  observable.box<CurrentServiceIdOption>(O.none, {
+    equals: O.getEq(S.Eq).equals,
+  } satisfies Eq<CurrentServiceIdOption>);
+
+export const connect = (
+  currentServiceId: IObservableValue<O.Option<NonEmptyString>>
+) =>
+  action((serviceId: NonEmptyString) =>
+    pipe(
+      currentServiceId.get(),
+      O.fold(() => currentServiceId.set(O.some(serviceId)), constVoid)
+    )
+  );
