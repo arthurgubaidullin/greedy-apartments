8cc461929d36288b99961fd7267c2740e6af13c2
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-26 10:16:56 +0000

    Move project

--- realtor-space/current-service/service-id/observable/jest.config.ts
+++ realtor-space/current-service/service-id/observable/jest.config.ts
@@ -1,12 +1,12 @@
 /* eslint-disable */
 export default {
   displayName: 'current-service-id-observable-in-realtor-space',
-  preset: '../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory:
-    '../../../coverage/realtor-space/observables/current-service-id',
+    '../../../../coverage/realtor-space/current-service/service-id/observable',
 };
