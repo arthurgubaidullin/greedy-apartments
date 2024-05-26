ca78b6adb4c4001beddea191c27371ba9fd1ef6a
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-26 14:09:05 +0000

    Move project

--- offer-space/offers/offer/id/jest.config.ts
+++ offer-space/offers/offer/id/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'offer-id-in-offer-space',
-  preset: '../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/offer-space/offer-id',
+  coverageDirectory: '../../../../coverage/offer-space/offers/offer/id',
 };
