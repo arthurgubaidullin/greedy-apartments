863be6b1979c7b94640ac1d8d51d03f06e5b1ceb
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-26 10:40:13 +0000

    Add type & function

--- offer-space/offer-struct/src/lib/offer-struct.ts
+++ offer-space/offer-struct/src/lib/offer-struct.ts
@@ -1,10 +1,14 @@
 import * as OfferId from '@ga/offer-id-in-offer-space';
 import * as _Eq from 'fp-ts/Eq';
+import { pipe } from 'fp-ts/function';
 import * as S from 'fp-ts/string';
 import * as t from 'io-ts';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface OfferStruct extends t.TypeOf<typeof codec> {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface OfferStructSimplified extends t.OutputOf<typeof codec> {}
 
 export const codec = t.readonly(
   t.strict({
@@ -17,3 +21,6 @@ export const Eq: _Eq.Eq<OfferStruct> = _Eq.struct({
   id: OfferId.Eq,
   name: S.Eq,
 });
+
+export const toJSON = (struct: OfferStruct): OfferStructSimplified =>
+  pipe(struct, codec.encode);
