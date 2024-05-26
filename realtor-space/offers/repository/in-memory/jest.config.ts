828eb8af2ed3749b6d0d429abb406e2f4a1364ec
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-26 13:27:46 +0000

    Move project

--- realtor-space/offers/repository/in-memory/jest.config.ts
+++ realtor-space/offers/repository/in-memory/jest.config.ts
@@ -1,11 +1,12 @@
 /* eslint-disable */
 export default {
   displayName: 'offers-repository-in-realtor-space',
-  preset: '../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/realtor-space/offers/repository',
+  coverageDirectory:
+    '../../../../coverage/realtor-space/offers/repository/in-memory',
 };
--- realtor-space/offers/repository/src/lib/offer-repository.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('offer-repository-in-realtor-space', () => {
-  it('should work', () => {
-    expect(1).toEqual(1);
-  });
-});
