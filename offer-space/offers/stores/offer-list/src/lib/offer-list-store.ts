b67b26ebfe14d698bfc118b90c12da2dbfcad363
Artur Gubaidullin <lapteuh@gmail.com>
2024-05-27 05:42:58 +0000

    Add function

--- offer-space/offers/stores/offer-list/src/lib/offer-list-store.ts
+++ offer-space/offers/stores/offer-list/src/lib/offer-list-store.ts
@@ -1,10 +1,11 @@
 import * as OfferStruct from '@ga/offer-struct-in-offer-space';
+import { ReadonlyObservable } from '@ga/readonly-observable';
 import * as _Eq from 'fp-ts/Eq';
 import * as O from 'fp-ts/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
 import { pipe } from 'fp-ts/function';
-import { IObservableValue, action, observable } from 'mobx';
+import { IObservableValue, action, computed, observable } from 'mobx';
 
 type T = O.Option<RNEA.ReadonlyNonEmptyArray<OfferStruct.OfferStruct>>;
 
@@ -17,6 +18,19 @@ export const update = (offerListStore: IObservableValue<T>) =>
   action((offerList: ReadonlyArray<OfferStruct.OfferStruct>): void =>
     pipe(offerList, O.fromPredicate(RA.isNonEmpty), (list) =>
       offerListStore.set(list)
+    )
+  );
+
+const monoid = O.getMonoid<RNEA.ReadonlyNonEmptyArray<OfferStruct.OfferStruct>>(
+  RNEA.getSemigroup()
+);
+
+export const merge = (list: ReadonlyArray<ReadonlyObservable<T>>) =>
+  computed(() =>
+    pipe(
+      list,
+      RA.map((list) => list.get()),
+      RA.reduce<T, T>(O.none, (b, a) => monoid.concat(b, a))
     )
   );
 
